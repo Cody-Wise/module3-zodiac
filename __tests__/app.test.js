4a477c84b7df825ace351c0c21fbe205500ccf59
Cody-Wise <cody@sharptackmedia.com>
2022-06-06 15:13:50 -0700

    Finished routes and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,29 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { zodiacs } = require('../data/zodiacs');
 
 describe('module3-zodiac routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('/zodiacs should return a list of zodiac signs', async () => {
+    const res = await request(app).get('/zodiacs');
+    const expected = zodiacs.map((zodiac) => {
+      return { id: zodiac.id, name: zodiac.name };
+    });
+    expect(res.body).toEqual(expected);
+  });
+
+  it('zoaidacs/:id should return zodiac details', async () => {
+    const res = await request(app).get('/zodiacs/1');
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    });
   });
 
   afterAll(() => {
